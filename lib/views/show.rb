073438962cbdd52358984dbc96bfdba7dc4c1b9d
annieherieau <annie.herieau@gmail.com>
2024-02-01 22:35:05 +0100

    view askplayer ok

--- lib/views/show.rb
+++ lib/views/show.rb
@@ -5,15 +5,35 @@ class Show
   
   def initialize
     welcome
-    @grid
   end
 
+  def ask_player_choice(player)
+    puts "#{player.avatar} #{player.name}, c'est ton tour : "
+  end
+
+  
+
+  def score_history(history)
+    puts "AFFICHAGE HISTORIQUE DES SCORES"
+    puts history
+  end
+
+ 
+
+  ##_________ METHODES OK 
+
+  # affichage du Plateau de jeu
   def board(board)
     skip_line
+    str = (' ' * 3)
+    (1..board.size).each do |num|
+       str += num.to_s.center(4, ' ')
+    end
+    puts str.center(50, ' ')
     puts ('-' * (board.size * 5)).center(50, ' ')
     board.set.each do |lettre , line|
       # str = lettre.center(3, ' ') + '|'
-      str = '|'
+      str = "#{lettre} |"
       line.each do |num, board_case|
         str += board_case.value.center(3, ' ') + '|'
       end 
@@ -22,25 +42,48 @@ class Show
       puts str.center(50, ' ')
       puts ('-' * (board.size * 5)).center(50, ' ')
     end
-    
   end
 
-  def score_history(hash)
-    puts "AFFICHAGE HISTORIQUE DES PARTIES"
-    puts hash
+   # appuyer pour continuer
+   def pause
+    skip_line
+    print 'Appuyer sur une touche pour continuer'
+    gets.chomp
   end
 
-  ##_________ METHODES OK 
-
-  # affiche le début de la nouvelle partie
-  def new_game(game)
-    text = "#{game.player1.avatar} #{game.player1.name.upcase}  vs  #{game.player2.avatar} #{game.player2.name.upcase}"
+  # affiche le titre de la partie
+  def game_title(app)
     clear
-    title('Nouvelle partie')
+    title("MORPION - Partie n°#{app.score_history[:count]}")
+    text = "#{app.player1.avatar} #{app.player1.name.upcase}  vs  #{app.player2.name.upcase} #{app.player2.avatar}"
     center_text(text)
     center_text('~' * (text.length + 10))
   end
 
+  # affiche le titre de la partie en cours
+  def round_game(app)
+    game = app.game
+    game_title(app)
+  end
+
+  # affiche l'écran d'accueil
+  def welcome
+    clear
+    title("JOUONS AU MORPION")
+  end
+
+  # affiche l'échran de fin
+  def bye
+    skip_line
+    title("BYE BYE")
+  end
+
+  # demande le nom du joueur
+  def ask_player_name(player, number)
+    print "#{player.avatar} Joueur n°#{number} : Quel est ton nom ? "
+  end
+
+  private
   # effacer l'écran
   def clear 
     system('clear')
@@ -70,20 +113,7 @@ class Show
     line
   end
 
-  # affiche l'écran d'accueil
-  def welcome
-    clear
-    title("JOUONS AU MORPION")
-  end
 
-  # demande le nom du joueur
-  def ask_player_name(player, number)
-    print "#{player.avatar} Joueur n°#{number} : Quel est ton nom ? "
-  end
 
-  # affiche l'échran de fin
-  def bye
-    skip_line
-    title("BYE BYE")
-  end
+ 
 end
