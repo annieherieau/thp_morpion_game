f2c424a813390f01fdb5e6fa33d7f042739e97a6
annieherieau <annie.herieau@gmail.com>
2024-02-01 22:34:04 +0100

    player get choice ok

--- lib/app/player.rb
+++ lib/app/player.rb
@@ -1,29 +1,24 @@
 # frozen_string_literal: true
 
 class Player 
-  attr_accessor :name, :number, :avatar, :sign, :case_to_play
+  attr_accessor :name, :number, :avatar, :sign, :choice
   
   def initialize(number)
     @name = ''
     @number = number
     @sign = number == 1 ? 'O' : 'X'
     @avatar = ['🐶', '🦄​', '🐯​', '🔥', '🍀', '🎲', '😸','🥰', '😁​', '😇', '😎', '🥳'].sample
-    @case_to_play = ''
-  end
-
-
-  # demander quelle case il veut jouer
-  def get_case_to_play
-    puts "gets.comp @case_to_play"
-    # verif case libre boardcase.is_free?
+    @choice = ''
   end
 
   ##_________ METHODES  OK 
+  # demander quelle case il veut jouer
+  def get_choice
+    @choice = gets.chomp.upcase while @choice == ''
+  end
 
   # demander son nom
   def get_name
     @name = gets.chomp while  @name == ''  
   end
-
-
 end
