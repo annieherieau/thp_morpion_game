74238f289a5ee7773e6962f7e3eb74f64285a016
annieherieau <annie.herieau@gmail.com>
2024-02-02 10:01:26 +0100

    encapsulation totale

--- app.rb
+++ app.rb
@@ -10,33 +10,37 @@ require "app/board_case"
 require "app/morpion_app"
 require "views/show"
 
-# ---- Début de l'application
-morpion = MorpionApp.new
-player1 = morpion.player1
-player2 = morpion.player2
+def run_app
+  # ---- Début de l'application
+  morpion = MorpionApp.new
+  player1 = morpion.player1
+  player2 = morpion.player2
 
-play_again = true
-# JOUER à MORPION : play_again == true
-while play_again
-  # GAME : game.is_over? == false
-  game = morpion.new_game
-  board = game.board
+  play_again = true
+  # JOUER à MORPION : play_again == true
+  while play_again
+    # GAME : game.is_over? == false
+    game = morpion.new_game
+    board = game.board
 
-  until game.is_over?
-    morpion.new_round
-    break if game.is_over?
+    until game.is_over?
+      morpion.new_round
+      break if game.is_over?
+    end
+    # --- fin GAME OVER : game.is_over? == true
+
+    # AFFICHER LE SCORE
+    # update score history
+    morpion.end_game
+
+    # PLAY AGAIN ? demander si nouvelle partie
+    play_again = morpion.play_again?
   end
-  # --- fin GAME OVER : game.is_over? == true
+  # ____ Fin JOUER à MORPION : play_again == false
 
-  # AFFICHER LE SCORE
-  # update score history
-  morpion.end_game
-
-  # PLAY AGAIN ? demander si nouvelle partie
-  play_again = morpion.play_again?
+  # afficher l'historique des scores
+  morpion.end
+  # ---- Fin de l'application
 end
-# ____ Fin JOUER à MORPION : play_again == false
 
-# afficher l'historique des scores
-morpion.end
-# ---- Fin de l'application
+run_app
